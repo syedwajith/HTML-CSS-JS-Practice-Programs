b26804f03517aca92e893ce46568226307e1314f
syedsathiq <syedsathiq123@gmail.com>
2023-01-27 13:59:08 +0530

    first commit

---
+++ Our Web App/Bus.php
@@ -0,0 +1,27 @@
+class Bus:
+    def __init__(self,conductor_name,total_seats,seats_booked,bus_registration_no,driver_name):
+        self.conductor_name=conductor_name
+        self.total_seats=total_seats
+        self.seats_booked=seats_booked
+        self.bus_registration_no=bus_registration_no
+        self.driver_name=driver_name
+
+    def print_bus_details(self):
+        print("printing Bus Details")
+        obj1.print_bus_details()
+        obj2.print_bus_details()
+
+obj1=Bus("john",50,40,"TN101","joe")
+obj2=Bus("michel",50,35,"TN102","allan")
+
+    
+
+       
+        
+
+    
+
+    
+
+    
+    
